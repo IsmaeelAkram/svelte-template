ac725fb301fa9128e1f180c8dff6eb74de8037b4
mahjestic <19865145+IsmaeelAkram@users.noreply.github.com>
2021-06-06 12:58:56 -0400

    first commit

---
+++ src/app.svelte
@@ -0,0 +1,14 @@
+<script>
+	let count = 100;
+	function increment() {
+		count++;
+	}
+</script>
+
+<main>
+	<div class="p-3">
+		<h1>Let's count!</h1>
+		<p>Current: {count}</p>
+		<button class="btn btn-primary" on:click={increment}>Increment</button>
+	</div>
+</main>
